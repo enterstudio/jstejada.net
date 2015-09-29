0a0381a0dadbc11bb7d3acc833781ddf6373418b
Juan Tejada <juans.tejada@gmail.com>
2015-09-29 12:01:31 -0400

    Adds initial package.json, deps and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const sassLoaders = [
+  'css',
+  'autoprefixer?browsers=last 2 version',
+  'sass?sourceMap&includePaths[]=' +
+    encodeURIComponent(path.resolve(__dirname, './src/styles', './node_modules'))
+];
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract("style", sassLoaders.join('!')) },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['babel?optional=es7.classProperties', 'eslint']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css', { allChunks: true  })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx'],
+    modulesDirectories: ['src', 'node_modules'],
+  },
+};
