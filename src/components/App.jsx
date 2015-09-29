b096cbd3c94c031467f35221f81a3c4dd2a97fcb
Juan Tejada <juans.tejada@gmail.com>
2015-09-29 12:44:49 -0400

    Adds initial version of site

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Header from 'components/Header';
+import 'styles/App';
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <Header />
+    );
+  }
+
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import 'styles/Header';
+
+export default class Header {
+
+  render() {
+    return <div className="Header">Hi there.</div>;
+  }
+}
