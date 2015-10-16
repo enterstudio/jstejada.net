a285c4364235c8e97582bab4122d01e559151c86
Juan Tejada <juans.tejada@gmail.com>
2015-10-15 19:44:39 -0500

    Updates webpack config formatting and adds publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const sassLoaders = [
   'css?sourceMap',
   'autoprefixer?browsers=last 2 version',
@@ -7,29 +7,31 @@ const sassLoaders = [
     'includePaths[]=' +
       encodeURIComponent(path.resolve(__dirname, './src/styles')) + '&' +
     'includePaths[]=' +
-      encodeURIComponent(path.resolve(__dirname, './node_modules'))
+      encodeURIComponent(path.resolve(__dirname, './node_modules')),
 ];
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, './dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath: '/',
   },
   module: {
     loaders: [
       {
         test: /\.s?css$/,
-        loader: ExtractTextPlugin.extract("style", sassLoaders.join('!')) },
+        loader: ExtractTextPlugin.extract("style", sassLoaders.join('!')),
+      },
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ['babel?optional=es7.classProperties', 'eslint']
-      }
-    ]
+        loaders: ['babel?optional=es7.classProperties', 'eslint'],
+      },
+    ],
   },
   plugins: [
-    new ExtractTextPlugin('[name].css', { allChunks: true  })
+    new ExtractTextPlugin('[name].css', {allChunks: true}),
   ],
   resolve: {
     extensions: ['', '.js', '.json', '.jsx', '.scss'],
